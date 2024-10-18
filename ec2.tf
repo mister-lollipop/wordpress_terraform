ed0ec7803628b0519d8422a2a3dd762ae8c9a322
veera reddy inakollu <ivreddy1422@gmail.com>
2024-10-18 11:05:58 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,20 +1,20 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "wordpress1" {
+resource "aws_instance" "wordpress2" {
   ami           = "ami-0a5c3558529277641"
   instance_type = "t2.micro"
   associate_public_ip_address = true
   user_data = file("data.sh")
   key_name = "veera"
-  vpc_security_group_ids= [aws_security_group.group.id]
+  vpc_security_group_ids= [aws_security_group.group1.id]
   tags = {
-    Name = "wordpress1" 
+    Name = "wordpress2" 
   }
 }
 # Security group to allow SSH and HTTP access
-resource "aws_security_group" "group" {
-  name        = "group"
+resource "aws_security_group" "group1" {
+  name        = "group1"
   
   ingress {
     from_port   = 22
